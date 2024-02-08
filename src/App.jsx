0af610defe4bd7debcb198d207bd433f29a5f088
sophanit <sophanit@sophanits-MacBook-Pro.local>
2024-02-08 16:36:57 +0700

    fix something

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,7 @@ import Container from "./components/Container/Container";
 const App = () => {
   return (
     <main className="w-full">
-      <Container>
-        <p>KIKILU</p>
-      </Container>
+      <Container></Container>
     </main>
   );
 };
